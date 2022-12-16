cb13e7b57685cc3fcc44de4e2e79980a68ac76e2
JBC <josmarbcristello@gmail.com>
2022-12-15 21:50:33 -0700

    Added possibility of generating white noise with generate_cwt

--- functions/generate_cwt.m
+++ functions/generate_cwt.m
@@ -1,23 +1,22 @@
 % Generates a CWT wavelet from time-series data (Converts a 1D signal to 2D images)
 % Author: Josmar Cristello
 %% Inputs
-%    data: Full path for "Results" object. Should contain a pressure and velocity sub-objects.
-%    mode: "Pressure" or "Velocity", and transforms the corresponding property. Defaults to pressure.
-%    location: "inlet", "outlet", or "both".
+%    data:        Full path for "Results" object. Should contain a pressure and velocity sub-objects.
+%    mode:        "Pressure" or "Velocity", and transforms the corresponding property. Defaults to pressure.
+%    location:    "inlet", "outlet", or "both".
 %    saveResults: if "true" saves the images. if "false", doesn't.
-%    fs: Sampling frequency (in Hz). Default value iz 2 Hz.
-%    wname: Mother wavelet name, defaults to Morlet (Gabor).
+%    fs:          Sampling frequency (in Hz). Default value iz 2 Hz.
+%    wname:       Mother wavelet name, defaults to Morlet (Gabor).
+%    whiteNoise:  The signal-to-noise ratio to be added as gaussian white noise. 0 disables it.
 %% Outputs
 %    N/A: 2x Image files (CWT), corresponding to the Outlet and Inlet transformation.
-%    Image name is based on 'data' name. Subfolder created is also based on
-%    'data' subfolder.
+%    Image name is based on 'data' name. Subfolder created is also based on 'data' subfolder.
 %% Notes
-%    The DC value obtained from Matlab is twice the actual DC value of the
-%    signal.
+%    The DC value obtained from Matlab is twice the actual DC value of the signal.
 %% TODO
 %% Main
   
-function [cwt_inlet, cwt_outlet] = generate_cwt(data, mode, location, saveResults, wname, fs)    
+function [cwt_inlet, cwt_outlet, output_folder] = generate_cwt(data, mode, location, saveResults, wname, fs, whiteNoise)    
     %% Function defaults    
     % Default value → pressure
     if nargin < 2
@@ -39,10 +38,19 @@ function [cwt_inlet, cwt_outlet] = generate_cwt(data, mode, location, saveResult
     if nargin < 6
         fs = 2; 
     end
+    % Default value → 0
+    if nargin < 7
+        whiteNoise = 0; 
+    end
 
     % Load the results object.
     Results = load(data);
     Results = Results.Results;
+    
+    %% Adds gaussian white noise to input, if the option is present
+    if whiteNoise > 0
+        Results = add_wgn(data, whiteNoise);
+    end
 
     %% Generating filename and output folder
     %  Output Filename is based on Input filename.
@@ -55,6 +63,7 @@ function [cwt_inlet, cwt_outlet] = generate_cwt(data, mode, location, saveResult
     verify_folder(output_folder);
     %output_folder = output_folder + cwt_folder + "/" + output_subfolder + "/";
     disp(output_folder);
+    output_folder = output_folder;
 
     %% Assumptions that depend on the Results object.
     inlet_index = 1;
