92b39a84b0e1a30abbf4d6c302313949670b8add
JBC <josmarbcristello@gmail.com>
2022-12-15 20:26:52 -0700

    Documentation for verify_folder

--- functions/verify_folder.m
+++ functions/verify_folder.m
@@ -1,8 +1,14 @@
+% This function checks if the folder(s) already exists. Creates if it
+% (they) don't, and does nothing if it (they) do.
+% Author: Josmar Cristello
+%% Inputs
+%    full_path: Full folder path.
+%% Outputs
+%    N/A: Creates folder(s)
+%% Main
 function verify_folder(full_path)
-%CREATE_FOLDER_IF Summary of this function goes here
-%   Detailed explanation goes here
-if not(isfolder(full_path))
-    mkdir(full_path)
-end
+    if not(isfolder(full_path))
+        mkdir(full_path)
+    end
 end
 
