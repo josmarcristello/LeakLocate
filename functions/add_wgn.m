9d55e625f0e2db76da349b4d8feabef2f8f1f9bf
JBC <josmarbcristello@gmail.com>
2022-12-15 21:49:56 -0700

    Update add_wgn.m

--- functions/add_wgn.m
+++ functions/add_wgn.m
@@ -9,9 +9,6 @@ function [results_noise] = add_wgn(data, snr)
 
     Results.Pressure = awgn(Results.Pressure, snr, 'measured');
     Results.Velocity = awgn(Results.Velocity, snr, 'measured');
-    %figure;
-    %plot(t,[x y]);
-    %legend('Original Signal','Signal with AWGN');
     results_noise = Results;
 end
 
